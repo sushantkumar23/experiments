b2e0222d19e3fb3b47c763e118024191a7d4f554
sushantkumar23 <sushantkumar23@gmail.com>
2020-08-22 11:46:41 +0530

    Adding GPT3 integration

--- app.py
+++ app.py
@@ -1,24 +1,48 @@
+# app.py
+import os
+import json
+import requests
+
 from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
+API_KEY = os.getenv('API_KEY')
+DAVINCI_URL = "https://api.openai.com/v1/engines/davinci/completions"
+
+headers = {
+	"Content-Type": "application/json",
+	"Authorization": "Bearer {}".format(API_KEY)
+}
+
+payload = {
+	"max_tokens": 60,
+	"temperature": 0,
+	"top_p": 1,
+	"n": 1,
+	"stream": False,
+	"logprobs": None,
+	"stop": "\n"
+}
+
 def gpt3(input):
-    return "This is GPT3. I got your input " + input
+	payload['prompt'] = input
+	response = requests.post(DAVINCI_URL, headers=headers, data=json.dumps(payload))
+	res_data = response.json()
+	output = res_data['choices'][0]['text'].strip()
+	return output
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/getResult', methods=['POST'])
 def getOutput():
-    ...
+	input = (request.form['input'])
+	output = gpt3(input)
 
-    input = (request.form['input'])
-    output = gpt3(input)
-
-    return render_template('output.html',output = output)
+	return render_template('output.html',output = output)
 
 
 if __name__ == '__main__':
-    app.run()
-
+	app.run()
